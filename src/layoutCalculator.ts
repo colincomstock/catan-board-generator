9a9c13d82ea5d9e9ad88e9f3c3dc113c1a88c395
colincomstock <95597475+colincomstock@users.noreply.github.com>
2025-06-23 13:59:01 -0700

    Update layoutCalculator.ts

--- src/layoutCalculator.ts
+++ src/layoutCalculator.ts
@@ -23,7 +23,30 @@ export default function generateBoard(): void {
         //ports: Port[];
     }
 
-    function generateTile(): Tile {
+    const tileDeck = [
+        { "id": 0,  "resource": "wood"   },
+        { "id": 1,  "resource": "wood"   },
+        { "id": 2,  "resource": "wood"   },
+        { "id": 3,  "resource": "wood"   },
+        { "id": 4,  "resource": "sheep"  },
+        { "id": 5,  "resource": "sheep"  },
+        { "id": 6,  "resource": "sheep"  },
+        { "id": 7,  "resource": "sheep"  },
+        { "id": 8,  "resource": "wheat"  },
+        { "id": 9,  "resource": "desert" },
+        { "id": 10, "resource": "wheat"  },
+        { "id": 11, "resource": "wheat"  },
+        { "id": 12, "resource": "wheat"  },
+        { "id": 13, "resource": "brick"  },
+        { "id": 14, "resource": "brick"  },
+        { "id": 15, "resource": "brick"  },
+        { "id": 16, "resource": "ore"    },
+        { "id": 17, "resource": "ore"    },
+        { "id": 18, "resource": "ore"    }
+      ]
+      
+
+    /*function generateTile(): Tile {
         let xCoord = 0
         let yCoord = Math.floor(Math.random() * 5)
         if (yCoord < 3) {
@@ -33,7 +56,7 @@ export default function generateBoard(): void {
             xCoord = Math.floor(Math.random() * (7 - yCoord))
         }
         console.log(`Random Coords = [${xCoord}, ${yCoord}]`)
-    }
+    }*/
 
 
 
@@ -57,7 +80,7 @@ export default function generateBoard(): void {
         return array;
     }
 
-    let sampleArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    let sampleArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 
     function random(seed: number) {
         const x = Math.sin(seed++) * 10000;
@@ -80,11 +103,40 @@ export default function generateBoard(): void {
     ]
 
     let newPort: Port = {coords: 2, resource: 'any'}
-    generateTile();
+    //generateTile();
 
     console.log(newPort);
 
 
     shuffle(sampleArray, 2345695);
     console.log(sampleArray)
+    function genBoard() {
+        const board: Board = {
+            tiles: []
+            //ports: []
+        }
+
+        for (let i = 0; i < sampleArray.length; i++) {
+            const tile = tileDeck[sampleArray[i]];
+            const coords = standardBoardLayout[i];
+            const newTile: Tile = {
+                coords: coords,
+                resource: tile.resource as ResourceType,
+                numberToken: null,
+                hasRobber: false,
+                tileID: tile.id as TileId
+            }
+            console.log(`Tile ID: ${tile.id}, Resource: ${tile.resource}, Coords: [${coords[0]}, ${coords[1]}]`);
+            console.log(newTile);
+            board.tiles.push(newTile);
+    
+        }
+
+        return board;
+
+    }
+
+    const newBoard = genBoard();
+    console.log("Board generated successfully.");
+    console.log(newBoard);
 }
